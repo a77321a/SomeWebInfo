5a6351dcae43f1597db97a2fd276824a3c122309
a77321a <1817169988@qq.com>
2020-05-23 16:41:31 +0800

    原生js

---
+++ 引入本地数据/data.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: 
+ * @Author: 
+ * @Date: 2020-05-23 15:29:14
+ * @LastEditors: 
+ * @LastEditTime: 2020-05-23 15:35:25
+ */
+var data = [
+  { name: "Ashwin", age: "20" },
+  { name: "Abhinandan", age: "20" }
+]
